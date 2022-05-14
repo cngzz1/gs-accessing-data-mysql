8ff867740e545561230a84af55ff3892c9bef820
Chris NG <cngzz1@gmail.com>
2022-05-14 21:33:51 +0800

    Testing generics

--- complete/src/main/java/com/example/accessingdatamysql/MainController.java
+++ complete/src/main/java/com/example/accessingdatamysql/MainController.java
@@ -21,7 +21,7 @@ public class MainController {
 		// @ResponseBody means the returned String is the response, not a view name
 		// @RequestParam means it is a parameter from the GET or POST request
 
-		User n = new User();
+		User<Object> n = new User<>();
 		n.setName(name);
 		n.setEmail(email);
 		userRepository.save(n);
@@ -29,7 +29,7 @@ public class MainController {
 	}
 
 	@GetMapping(path="/all")
-	public @ResponseBody Iterable<User> getAllUsers() {
+	public @ResponseBody Iterable<User<Object>> getAllUsers() {
 		// This returns a JSON or XML with the users
 		return userRepository.findAll();
 	}
---
+++ complete/src/main/java/com/example/accessingdatamysql/UserDto.java
@@ -0,0 +1,52 @@
+package com.example.accessingdatamysql;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class UserDto implements Serializable {
+    private final Integer id;
+    private final String name;
+    private final String email;
+
+    public UserDto(Integer id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserDto entity = (UserDto) o;
+        return Objects.equals(this.id, entity.id) &&
+                Objects.equals(this.name, entity.name) &&
+                Objects.equals(this.email, entity.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s(id = %d, name = %s, email = %s)", getClass().getSimpleName(), id, name, email);
+    }
+}
